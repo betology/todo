aa714922457b7aef1cf210455c155b2c5b4bc1be
betology <betology@gmail.com>
2023-07-09 19:35:29 -0600

    Finished initial CLI implementation

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -14,7 +14,7 @@ const todoFileName = ".todo.json"
 func main ) {
 
     // Define an items list
-    l :=&todo.List{}
+    l := &todo.List{}
 
 
     // Use the Get method to read to do items from file
@@ -22,3 +22,32 @@ func main ) {
         fmt.Fprintln(os.Stderr, err)
         os.Exit(1)
     }
+
+    // Decide what to do based on the number of arguments provided
+    switch {
+    	// For no extra arguments, print the list
+    	case len(os.Args) == 1:
+	    // List current to do items
+	    for _, item := range *l {
+		    fmt.Println(item.Task)
+	    }
+
+    	// Concatenate all provided arguments with a space and 
+    	// add to the list asan item 
+    	default:
+    	// Concatenate all arguments with a space
+		item := strings.Join(os.Args[1:], " ")
+		// Add the task
+		l.Add(item)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+	}
+	}
+}
+
+
+
+
