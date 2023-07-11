ff6191089e63d213efffad361692e912a9664c93
b3t0 <b3t0@botox>
2023-07-11 10:48:12 -0600

    Handling multiple command-line options

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,53 +1,69 @@
-package main 
+package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
+	"flag"
+	"fmt"
+	"os"
 
-    "github.com/betology/todo"
+	"github.com/betology/todo"
 )
 
 // Hardcoding the file name
 const todoFileName = ".todo.json"
 
 func main() {
+	// Parsing comman line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all task")
+	complete := flag.Int("complete", 0, "Item to be completed")
 
-    // Define an items list
-    l := &todo.List{}
+	flag.Parse()
 
+	// Define an items list
+	l := &todo.List{}
 
-    // Use the Get method to read to do items from file
-    if err := l.Get(todoFileName); err != nil {
-        fmt.Fprintln(os.Stderr, err)
-        os.Exit(1)
-    }
+	// Use the Get command  to read to do items from file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 
-    // Decide what to do based on the number of arguments provided
-    switch {
-    	// For no extra arguments, print the list
-    	case len(os.Args) == 1:
-	    // List current to do items
-	    for _, item := range *l {
-		    fmt.Println(item.Task)
-	    }
+	// Decide what to do based on the provided flags
+	switch {
+	case *list:
+		// List current to do items
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
 
-    	// Concatenate all provided arguments with a space and 
-    	// add to the list asan item 
-    	default:
-    	// Concatenate all arguments with a space
-		item := strings.Join(os.Args[1:], " ")
-		// Add the task
-		l.Add(item)
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
-	}
+		}
+
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		// Invalid flag privided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
-
-
-
-
